8b018583d030ba8c904754df1e1e55dae41b7d94
Kate Summerill <kaitlynsummerill@gmail.com>
2020-01-08 01:48:03 +0000

    Automatically backed up by Learn

--- lib/post.rb
+++ lib/post.rb
@@ -1,9 +1,10 @@
 class Post
 
-  attr_accessor :title, :Author
+  attr_accessor :title, :author
 
-  def initialize
+  def initialize(title, author)
     @title = title
+    @author = author
   end
 
 
